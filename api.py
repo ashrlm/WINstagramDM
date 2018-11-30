0964030715451002fdce62789ebbe250fb5f1724
Asher Matheson <ashrlmthsn@Gmail.com>
2018-12-01 09:36:45 +1300

    Added getMessages

--- api.py
+++ api.py
@@ -6,17 +6,20 @@ class User: #Setup custom user class
         self.api = InstagramAPI(usr_name, password)
         if not self.api.login():
             raise ValueError("Couldn't login")
-            
+
+        InstagramAPI.USER_AGENT = 'Instagram 39.0.0.19.93 Android (5000/5000.0; 1dpi; 1x1; noname; noname; noname; noname)'
+        #Setup custom UA to ensure reading dms allowed
+
     def sendMessage(self, target_user, msgText):
         if type(target_user[0]) != 'int':
             target_user = self.api.searchUsername(target_user)
             target_user = self.api.LastJson["user"]["pk"]
-        
+
         target_user = str(target_user)
-            
+
         target_user = '[[{}]]'.format(','.join([target_user]))
         url = 'direct_v2/threads/broadcast/text/'
-    
+
         data = {
             'text': msgText,
             '_uuid': self.api.uuid,
@@ -25,5 +28,9 @@ class User: #Setup custom user class
             '_uid': self.api.username_id,
             'action': 'send_item',
             'client_context': self.api.generateUUID(True)}
-    
+
         return self.api.SendRequest(url, data)
+
+    def getMessages(self):
+        self.api.getv2Inbox()
+        print(self.api.LastResponse.content)
