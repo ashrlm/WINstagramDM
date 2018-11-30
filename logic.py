0502d4e6317852f2101a2f0d08da6e44f1d8c294
Emlyn Matheson <emlynmatheson@Gmail.com>
2018-11-30 19:47:41 +1300

    Added DM functionality

---
+++ logic.py
@@ -0,0 +1,30 @@
+from InstagramAPI import InstagramAPI
+
+class User: #Setup custom user class
+    def __init__(self, usr_name, password):
+        self.name = usr_name
+        self.api = InstagramAPI(usr_name, password)
+        if not self.api.login():
+            raise TypeError("Couldn't login")
+        
+    def sendMessage(self, target_user, msgText):
+        if type(target_user[0]) != 'int':
+            target_user = self.api.searchUsername(target_user)
+            target_user = self.api.LastJson["user"]["pk"]
+        
+        target_user = str(target_user)
+        print(type(target_user))
+            
+        target_user = '[[{}]]'.format(','.join([target_user]))
+        url = 'direct_v2/threads/broadcast/text/'
+    
+        data = {
+            'text': msgText,
+            '_uuid': self.api.uuid,
+            '_csrftoken': self.api.token,
+            'recipient_users': target_user,
+            '_uid': self.api.username_id,
+            'action': 'send_item',
+            'client_context': self.api.generateUUID(True)}
+    
+        return self.api.SendRequest(url, data)
