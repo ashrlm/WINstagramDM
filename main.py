b4a08c53acb1ba981e730ed17a768bb1f44b5bfd
Emlyn Matheson <emlynmatheson@Gmail.com>
2018-12-01 09:36:31 +1300

    Name fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import os
 import getpass
 
-import logic
+import api
 
 def main():
     print("Welcome to WINstagram. Currently all this is is a chat client, but it will become a full IG client!")
@@ -11,12 +11,12 @@ def main():
     print("2: Chat mode")
     while True:
         try:
-            usr = logic.User(input("Username: "), getpass.getpass())
+            usr = api.User(input("Username: "), getpass.getpass())
             break
-            
+
         except ValueError:
             pass
-            
+
     while True:
         mode = input("Mode: ")
         if mode == '1':
@@ -27,29 +27,29 @@ def main():
                 delay = input("Delay between messages: ")
                 if not delay:
                     delay = 0
-                    
+
                 while True:
                     try:
                         usr.sendMessage(target, text)
                         time.sleep(delay)
                     except KeyboardInterrupt:
                         break
-                
+
                 continue
-                    
+
             else:
                 num_iters = input("Number of Messages: ")
                 for i in range(int(num_iters)):
                     usr.sendMessage(target, text)
-            
+
             continue
-        
+
         elif mode == '2':
             print("Unavaliable")
-        
+
         else:
             print("Invalid mode")
-    
+
 
 if __name__ == "__main__":
     main()
